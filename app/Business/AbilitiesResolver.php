364a9b264da80e983cc10bb4341e110954f6d6e0
david <davidsanchezdiaz72@gmail.com>
2024-04-10 18:50:04 +0200

    Agregada documentación y mejoras en la resolución de habilidades en AbilitiesResolver

---
+++ app/Business/AbilitiesResolver.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Business;
+
+use App\Models\User;
+
+class AbilitiesResolver {
+
+    /**
+     * Resuelve las habilidades del usuario según su rol y dispositivo.
+     *
+     * @param User $user El usuario para el que se resolverán las habilidades.
+     * @param string $device El dispositivo para el que se resolverán las habilidades.
+     * @return array Las habilidades resueltas para el usuario y el dispositivo.
+     */
+    public static function resolve(User $user, $device){
+
+        // Verificar si el usuario es un cliente
+        if ($user->role == 'client') {
+            // Si es un cliente, resolver habilidades específicas para clientes
+            return static::resolveForClient($device);
+        }
+    }
+
+    /**
+     * Resuelve las habilidades específicas para un cliente según el dispositivo.
+     *
+     * @param string $device El dispositivo para el que se resolverán las habilidades.
+     * @return array Las habilidades resueltas para el cliente y el dispositivo.
+     */
+    public static function resolveForClient($device){
+        // Utilizar un operador 'match' para asignar habilidades según el dispositivo
+        return match ($device) {
+            'watch' => [
+                'establishment:show',
+            ],
+            default => [
+                'establishment:show',
+                'orders:create',
+            ]
+        };
+    }
+}
--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,18 +6,27 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
+use App\Business\AbilitiesResolver;
 
 class LoginController extends Controller
 {
-
+    /**
+     * Maneja la lógica de inicio de sesión.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function login(){
         // Buscar el usuario en la base de datos utilizando su dirección de correo electrónico
         $user =  User::where('email', request('email'))->first();
 
         // Verificar si se encontró un usuario y si la contraseña proporcionada coincide
         if ($user && Hash::check(request('password'), $user->password)) {
-            // Generar un token de autenticación para el usuario
-            $token = $user->createToken('login');
+
+            // Resolver las habilidades del usuario para el dispositivo dado
+            $abilities = AbilitiesResolver::resolve($user, request('device'));
+            
+            // Crear un token de autenticación para el usuario con las habilidades resueltas
+            $token = $user->createToken('login', $abilities);
 
             // Devolver el token en formato JSON como respuesta
             return response()->json([
@@ -30,5 +39,4 @@ class LoginController extends Controller
             'message' => 'Invalid credentials',
         ], 401);
     }
-
 }
