34a662b9658ac59567df1376cdb5eb5f5b056d4f
david <davidsanchezdiaz72@gmail.com>
2024-04-11 16:32:36 +0200

    Agrupar rutas y variables de entorno en Insomnia

--- routes/api.php
+++ routes/api.php
@@ -6,27 +6,28 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\EstablismentsController;
 
+Route::middleware('auth:sanctum')->group(function (){
 
-Route::get('/establishments', [EstablismentsController::class, 'index']);
-/*
-// Define una ruta para crear órdenes
-Route::middleware('auth:sanctum')->post('orders', function (){
-    // Aborta la solicitud a menos que el usuario autenticado tenga permisos para crear órdenes
-    abort_unless( Auth::user()->tokenCan('orders:create'), 403, "You dont't have premissions to perform this action.");
-    // Devuelve un mensaje de éxito si se creó la orden
-    return [
-        'message' => 'Order created',
-    ];
-});
-*/
-// Definición de ruta para el inicio de sesión
-Route::post('login', [LoginController::class, 'login']);
+    // Ruta para el inicio de sesión
+    Route::post('login', [LoginController::class, 'login']);
 
-// Ruta protegida que requiere autenticación de Sanctum
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    // Devuelve el usuario autenticado actualmente
-    return Auth::user();
-    
-    // También se puede devolver el usuario a través del objeto Request
-    // return $request->user();
+    // Ruta para obtener la lista de establecimientos
+    Route::get('/establishments', [EstablismentsController::class, 'index']);
+
+    // Ruta para obtener los detalles del usuario autenticado
+    Route::get('/user', function (Request $request) {
+        return Auth::user();
+    });
+
+    // Ruta para crear órdenes
+    Route::post('orders', function (){
+        // Aborta la solicitud a menos que el usuario autenticado tenga permisos para crear órdenes
+        abort_unless( Auth::user()->tokenCan('orders:create'), 403, "You don't have permissions to perform this action.");
+        // Devuelve un mensaje de éxito si se creó la orden
+        return [
+            'message' => 'Order created',
+        ];
+    });
+
 });
+
