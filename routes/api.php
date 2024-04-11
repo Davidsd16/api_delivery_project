41c69c89e559595ea161581e380a5c437d297d00
david <davidsanchezdiaz72@gmail.com>
2024-04-11 11:06:04 +0200

    Se define una ruta POST llamada 'orders' para crear órdenes.

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,17 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 
+
+// Define una ruta para crear órdenes
+Route::post('orders', function (){
+    // Aborta la solicitud a menos que el usuario autenticado tenga permisos para crear órdenes
+    abort_unless( Auth::user()->tokenCan('orders:create'), 403, "You dont't have premissions to perform this action.");
+    // Devuelve un mensaje de éxito si se creó la orden
+    return [
+        'message' => 'Order created',
+    ];
+});
+
 // Definición de ruta para el inicio de sesión
 Route::post('login', [LoginController::class, 'login']);
 
